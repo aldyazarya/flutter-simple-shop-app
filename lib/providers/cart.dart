0fe7a1bc7f77e1541464588e1f86c2d0ea07c9a1
aldyazarya <aldy1612@gmail.com>
2019-12-18 18:06:23 +0800

    adding snackbar and UNDO function

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -33,7 +33,6 @@ class Cart with ChangeNotifier {
     return total;
   }
 
-
   void addItem(
     String productId,
     double price,
@@ -65,6 +64,26 @@ class Cart with ChangeNotifier {
 
   void removeItem(String productId) {
     _items.remove(productId);
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity - 1,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 
