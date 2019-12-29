000e1ba9fe0064978ed2a384a541d992856b12b0
aldyazarya <aldy1612@gmail.com>
2019-12-30 01:59:31 +0800

    adding user signup

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
-import './screens/cart_screen.dart';
+
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
+import './providers/auth.dart';
+
+import './screens/product_detail_screen.dart';
+import './screens/cart_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/user_product_screen.dart';
 import './screens/edit_product_screen.dart';
@@ -27,6 +30,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Orders(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Auth(),
         ),
       ],
       child: MaterialApp(
---
+++ lib/providers/auth.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expireDate;
+  String _userId;
+
+  Future<void> signUp(String email, String password) async {
+    const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdysuP4GfGILr5SysSQ2i4_yVBd8h6Q9s';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+}
