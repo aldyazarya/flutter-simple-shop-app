9bc285b63ac57144cfd7704fbd4e28c47968bb6b
aldyazarya <aldy1612@gmail.com>
2019-12-30 20:49:50 +0800

    handling authentication error

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expireDate;
@@ -11,17 +13,26 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegement) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegement?key=AIzaSyCdysuP4GfGILr5SysSQ2i4_yVBd8h6Q9s';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      // print(json.decode(response.body));
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signUp(String email, String password) async {
@@ -31,5 +42,4 @@ class Auth with ChangeNotifier {
   Future<void> signIn(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
   }
-
 }
