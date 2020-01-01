fc562aaf04a297c6ce1554e6ab53b90697d9cfca
aldyazarya <aldy1612@gmail.com>
2020-01-02 03:57:56 +0800

    adding the token to all request

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -24,12 +24,12 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
     final url =
-        'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json';
+        'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json?auth=$token';
     try {
       final response = await http.patch(
         url,
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -91,7 +91,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    const url = 'https://shop-app-flutter-98bb1.firebaseio.com/products.json';
+    final url = 'https://shop-app-flutter-98bb1.firebaseio.com/products.json?auth=$authToken';
     try {
       final response = await http.post(
         url,
@@ -122,7 +122,7 @@ class Products with ChangeNotifier {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       final url =
-          'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json';
+          'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json?auth=$authToken';
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -138,7 +138,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> deleteProduct(String id) async {
-    final url = 'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json';
+    final url = 'https://shop-app-flutter-98bb1.firebaseio.com/products/$id.json?auth=$authToken';
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
 
